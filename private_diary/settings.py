ac825dfd1c58047dcaa52e272854357da35c4db5
jun0618-10 <lingmucong84@gmail.com>
2024-03-11 20:30:10 +0900

    再修正

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -36,7 +36,7 @@ LOGGING={
         'file':{
             'level':'INFO',
             'class':'logging.handlers.TimedRotatingFileHandler',
-            'filename':os.path.join(BASE_DIR,'logs_django.log'),
+            'filename':os.path.join(BASE_DIR,'logs/django.log'),
             'formatter':'prod',
             'when':'D',
             'interval':1,
